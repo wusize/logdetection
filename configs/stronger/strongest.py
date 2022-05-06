26635621d4374ee7fdf4638fcb941b06db6c6fe6
wusize <wusize@sensetime.com>
2022-05-07 05:01:48 +0800

    'final'

--- configs/stronger/strongest.py
+++ configs/stronger/strongest.py
@@ -1,4 +1,5 @@
 _base_ = '../swinb_12x_800-1400_anchor_bs2x8_coco_autov2_albu_mixup_advance.py'
+num_classes = 50
 model = dict(
     type='CBCascadeRCNN',
     pretrained='models/swin_base_patch4_window7_224.pth',
@@ -181,11 +182,10 @@ model = dict(
         rpn=dict(
             nms_across_levels=False,
             nms_pre=5000,
-            nms_post=5000,
             max_per_img=5000,
             nms=dict(type='nms', iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=dict(
-            score_thr=0.001,
-            nms=dict(type='soft_nms', iou_threshold=0.5),
-            max_per_img=100)))
+            score_thr=0.0001,
+            nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.0001),
+            max_per_img=300)))
