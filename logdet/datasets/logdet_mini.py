136b84f3ed9555e94d9a9f9f922eb0e8e588f25a
wusize <wusize@sensetime.com>
2022-05-17 19:35:58 +0800

    'bug'

--- logdet/datasets/logdet_mini.py
+++ logdet/datasets/logdet_mini.py
@@ -3,13 +3,13 @@ from mmdet.datasets.api_wrappers import COCO
 
 
 class LogDetMini(CocoDataset):
-    CLASSES = (
-        'FESTO', 'starbucks', 'castrol', 'hellokitty', 'SANDVIK', 'alexandermcqueen', 'balabala', 'THINKINGPUTTY', 'CCTV',
-        'puma', 'piaget', 'givenchy', 'naturerepublic', 'pandora', 'chowtaiseng', 'kans', 'joyong', 'apple', 'lenovo',
-        'sergiorossi', 'joeone', 'dove', 'pigeon', 'keds', 'simon', 'burberry', 'wechat', 'thermos', 'asics', 'doraemon',
-        'brioni', 'rejoice', 'charles_keith', 'Jmsolution', 'musenlin', 'GOON', 'gentlemonster', 'hotwind', 'UnderArmour',
-        'alibaba', 'tesla', 'gloria', 'ThomasFriends', 'fendi', 'camel', 'Thrasher', 'yuantong', 'chromehearts',
-        'pepsicola', 'playboy')
+    # CLASSES = (
+    #     'FESTO', 'starbucks', 'castrol', 'hellokitty', 'SANDVIK', 'alexandermcqueen', 'balabala', 'THINKINGPUTTY', 'CCTV',
+    #     'puma', 'piaget', 'givenchy', 'naturerepublic', 'pandora', 'chowtaiseng', 'kans', 'joyong', 'apple', 'lenovo',
+    #     'sergiorossi', 'joeone', 'dove', 'pigeon', 'keds', 'simon', 'burberry', 'wechat', 'thermos', 'asics', 'doraemon',
+    #     'brioni', 'rejoice', 'charles_keith', 'Jmsolution', 'musenlin', 'GOON', 'gentlemonster', 'hotwind', 'UnderArmour',
+    #     'alibaba', 'tesla', 'gloria', 'ThomasFriends', 'fendi', 'camel', 'Thrasher', 'yuantong', 'chromehearts',
+    #     'pepsicola', 'playboy')
 
     def load_annotations(self, ann_file):
         """Load annotation from COCO style annotation file.
@@ -24,7 +24,7 @@ class LogDetMini(CocoDataset):
         self.coco = COCO(ann_file)
         # The order of returned `cat_ids` will not
         # change with the order of the CLASSES
-        # self.CLASSES = [cat['name'] for cat in self.coco.cats.values()]
+        self.CLASSES = [cat['name'] for cat in self.coco.cats.values()]
         self.cat_ids = self.coco.get_cat_ids(cat_names=self.CLASSES)
 
         self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
